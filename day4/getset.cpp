0e6b3491e8e532ef8943279ebd0fc6f6f00c8a27
J GAUTHAM <jgautham42@gmail.com>
2025-05-24 15:39:15 +0530

    day4

--- day4/getset.cpp
+++ day4/getset.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
-    int a;
-    cout << "Enter a number: ";
-    cin >> a;
-    for (int i = 0; i < 25; i++)
-    {
-        cout << " enna" << endl;
-    }
-    return 0;
+class items
+{
+private:
+    int itemno;
+    float price;
+    int quantity;
+public:
+    void setitemno(int x)
+{
+    itemno = x;
+}
+    void setprice(float y)
+{
+    price = y;
+}
+    int getitemno()
+{
+    return itemno;
+}
+    float getprice()
+{
+    return price;
+}
+int setquantity(int z)
+{
+    quantity = z; 
+}
+int getquantity()
+{
+    return quantity;
+}
 };
+
+int main()
+{
+    items i1;
+    i1.setitemno(101);
+    i1.setprice(99.99);
+    i1.setquantity(25);
+    cout << "Item number: " << i1.getitemno() << endl;
+    cout << "Price: " << i1.getprice() << endl;
+    cout << "Quantity: " << i1.getquantity() << endl;
+    return 0;
+}
