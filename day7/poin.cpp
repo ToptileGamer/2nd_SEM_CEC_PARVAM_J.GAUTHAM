6e6022bed810ce6aef8607173428a52ec55bda43
J GAUTHAM <jgautham42@gmail.com>
2025-05-28 15:45:43 +0530

    day7

---
+++ day7/poin.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+class Dog
+{
+public:
+    void bark()
+    {
+        cout << "Woof! Woof!" << endl;
+    }
+};
+
+int main()
+{
+    Dog *d = new Dog(); 
+    d->bark();
+    delete d; 
+    return 0;
+}
